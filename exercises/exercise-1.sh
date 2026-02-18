8482a2c2adb6b471334dedd2c4281cfda06c00b1
Ben Cöppicus <ben.coeppicus@bencoepp.de>
2026-02-18 11:34:24 +0100

    Add additional Docker commands and custom index for Nginx

--- exercises/exercise-1.sh
+++ exercises/exercise-1.sh
@@ -3,3 +3,15 @@ sudo docker run -d -p 80:80 nginx
 sudo docker ps
 
 curl http://localhost:80
+
+sudo docker images
+
+sudo docker ps
+
+sudo docker run -d -p 81:80 nginx
+
+cat > index.html <<'EOF'
+<h1>Hello from my custom index</h1>
+EOF
+
+sud0 docker cp ./index.html <container_id_or_name>:/usr/share/nginx/html/index.html
