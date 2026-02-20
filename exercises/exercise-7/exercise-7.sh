ff0501349ba26f881e661aa2ef2688cc37fe1cdf
Ben Cöppicus <ben.coeppicus@bencoepp.de>
2026-02-20 09:39:21 +0100

    Add exercise 7 script for Docker rootless installation and configuration

--- exercises/exercise-7/exercise-7.sh
+++ exercises/exercise-7/exercise-7.sh
@@ -2,3 +2,16 @@ sudo groupadd docker
 sudo usermod -aG docker $USER
 newgrp docker
 docker ps
+
+sudo apt install uidmap
+id -u
+whoami
+grep ^$(whoami): /etc/subuid
+grep ^$(whoami): /etc/subgid
+sudo systemctl disable --now docker.service docker.socket
+sudo rm /var/run/docker.sock
+sudo apt update
+
+sudo apt-get install -y docker-ce-rootless-extras
+dockerd-rootless-setuptool.sh install --force
+docker info
